5b4616d2a2453098c4aabaad04fbeadbed63cd31
reneevhuang <renee.v.huang@gmail.com>
2018-01-22 17:12:25 -0600

    added more instructions

--- Game.java
+++ Game.java
@@ -7,6 +7,8 @@ import java.util.*;
  */
 public class Game
 {
+    private int[][] playerMoves = new int[3][3];
+
     /**
      * TODO: 
      * 1. Initialize an int[3][3] and name the int array "game." make sure this is a class variable
@@ -22,6 +24,7 @@ public class Game
     }
 
     /**
+     * TODO:
      * 1. Create a for loop that would iterate the number of rounds played. Round number is indicated by the class variable "rounds"
      * 2. Create a while loop inside the for loop that runs while anyMovesLeft() is true. 
      * 3. Call player1Move()
@@ -33,9 +36,10 @@ public class Game
     }
 
     /**
+     * TODO:
      * 1. Get player 1 move of the row based on location of mouse clicked by calling getRowClicked() and setting it to an instance int called "row"
      *  Get player 1 move of the column based on location of mouse clicked by calling getColumnClicked() and setting it to an instance int called "column"
-     * 2. Make sure that the move is validate by checking if the
+     * 2. Add move to the playerMoves[row][column] by changing the value to 1.
      */
     public void player1Move()
     {
@@ -44,8 +48,19 @@ public class Game
 
     /**
      * TODO:
-     * Checks if there are any winners by calling didXWin() and didOWin(); if either returns true, the method should return false
-     * If there are no one, check the class variable game[][] to see if there are any moves left; create a for loop that iterates through the array to see if there are any
+     * 1. Get player 2 move of the row based on location of mouse clicked by calling getRowClicked() and setting it to an instance int called "row"
+     *  Get player 2 move of the column based on location of mouse clicked by calling getColumnClicked() and setting it to an instance int called "column"
+     * 2. Add move to the playerMoves[row][column] by changing the value to 2.
+     */
+    public void player2Move()
+    {
+
+    }
+
+    /**
+     * TODO:
+     * 1. Checks if there are any winners by calling didXWin() and didOWin(); if either returns true, the method should return false
+     * 2. If there are no one, check the class variable game[][] to see if there are any moves left; create a for loop that iterates through the array to see if there are any
      * empty spaces left
      */
     public boolean anyMovesLeft()
@@ -54,13 +69,16 @@ public class Game
     }
 
     /**
-     * Call 
+     *  
      */
     public int getRowClicked()
     {
         return -1;
     }
 
+    /**
+     * 
+     */
     public int getColumnClicked()
     {
         return -1;
